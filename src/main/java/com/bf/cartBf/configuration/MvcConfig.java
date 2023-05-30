9ad2371177acdc93531009719e3d2227ab14efe0
saifat <ivan@vizgalin.ru>
2023-05-30 08:43:08 +0300

    - add new images - add docker-compose.yaml - correcting property for BD - add snakeyaml dependency for cansel transitive

--- src/main/java/com/bf/cartBf/configuration/MvcConfig.java
+++ src/main/java/com/bf/cartBf/configuration/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.bf.cartBf.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,5 +10,9 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("main");
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 }
